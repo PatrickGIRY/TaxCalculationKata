d9ecc2319191287c5ed643788c209b172e8137ff
patrickgiry <patrickgiry@gmail.com>
2025-05-14 07:11:06 +0200

    Implements `CommandLinesParser`

--- src/main/java/katas/tax/calculation/functions/CommandLinesParser.java
+++ src/main/java/katas/tax/calculation/functions/CommandLinesParser.java
@@ -1,11 +1,35 @@
 package katas.tax.calculation.functions;
 
 import katas.tax.calculation.domain.CommandLine;
+import katas.tax.calculation.domain.Product;
+import katas.tax.calculation.domain.Quatity;
+import katas.tax.calculation.domain.UnitPriceExcludingTax;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class CommandLinesParser {
+    private static final Pattern LINE_PATTERN = Pattern.compile("(\\d+)\\s+(.*?)\\s*à\\s*(\\d+(?:\\.\\d+)?)");
+
     public List<CommandLine> parse(String commandLines) {
-        throw new RuntimeException("Not yet implemented");
+        if (commandLines == null || commandLines.isEmpty()) {
+            throw  new IllegalArgumentException(String.format("Invalid input [ %s ]", commandLines));
+        }
+        return commandLines.lines()
+                .map(CommandLinesParser::parseLine)
+                .toList();
+    }
+
+    private static CommandLine parseLine(String input) {
+        final var matcher = LINE_PATTERN.matcher(input);
+        if (matcher.lookingAt() && matcher.groupCount() == 3) {
+            return new CommandLine(
+                    new Quatity(Integer.parseInt(matcher.group(1))),
+                    new Product(matcher.group(2)),
+                    new UnitPriceExcludingTax(matcher.group(3))
+            );
+        } else {
+            throw new IllegalArgumentException(String.format("Invalid input line [ %s ]", input));
+        }
     }
 }
---
+++ src/test/java/katas/tax/calculation/functions/CommandLinesParserTest.java
@@ -0,0 +1,63 @@
+package katas.tax.calculation.functions;
+
+import katas.tax.calculation.domain.CommandLine;
+import katas.tax.calculation.domain.Product;
+import katas.tax.calculation.domain.Quatity;
+import katas.tax.calculation.domain.UnitPriceExcludingTax;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CommandLinesParserTest {
+
+    private CommandLinesParser commandLinesParser;
+
+    @BeforeEach
+    void setUp() {
+        commandLinesParser = new CommandLinesParser();
+    }
+
+    @Test
+    void should_parse_a_command_line() {
+
+        final var input = "1 flacon de parfum importé à 27.99";
+
+        List<CommandLine> commandLines = commandLinesParser.parse(input);
+
+        assertThat(commandLines).containsExactly(
+          new CommandLine(new Quatity(1), new Product("flacon de parfum importé"), new UnitPriceExcludingTax("27.99"))
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "flacon de parfum importé à 27.99",
+            "flacon de parfum importé 27.99",
+            "1 flacon de parfum importé 27.99",
+            "1 flacon de parfum importé à"
+    })
+    void fail_on_invalid_input_line(String invalidInput) {
+
+        assertThatThrownBy(() -> commandLinesParser.parse(invalidInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Invalid input line [ %s ]", invalidInput);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    void fail_on_invalid_input(String invalidInput) {
+
+        assertThatThrownBy(() -> commandLinesParser.parse(invalidInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Invalid input [ %s ]", invalidInput);
+    }
+}
